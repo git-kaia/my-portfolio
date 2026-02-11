bf34c6ccff4744c109687df7783b9f9e0cb66b4b
Kaia Emilie <149586229+git-kaia@users.noreply.github.com>
2026-02-11 15:34:56 +0100

    Enhancement * Code icon added to Home page card for decoration * Fix: Broken link     * Home page card is now a link to the About page * Colors updated

---
+++ src/components/svg/Code.jsx
@@ -0,0 +1,26 @@
+// components/svg/Code.jsx
+
+import { StripePattern } from "./StripePattern";
+
+export default function Code({ className = "" }) {
+
+    const pattern = StripePattern();
+
+    return (
+        <svg
+            className={className}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1307.71 980.72"
+        >
+            <defs>{pattern.defs}</defs>
+
+            {/* GitHub Logo */}
+            <path
+                d="M163.99,490.36L490.57,163.79h-163.29L.71,490.36l326.57,326.57h163.29L163.99,490.36ZM980.42,163.79h-163.28l326.57,326.57-326.57,326.57h163.28l326.58-326.57-326.58-326.57ZM694.67.5l-204.11,979.72h122.46L817.14.5h-122.47"
+                fill={`url(#${pattern.id})`}
+                stroke="var(--accentPrimary)"
+                strokeWidth="28"
+            />
+        </svg>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 // pages/Home.jsx
+import { Link } from 'react-router-dom';
 import PageFadeTransition from "../animations/PageFadeTransition";
 import PaperFan from "../components/cards/PaperFan";
-import Screens from "../components/svg/Screens";
+import Code from "../components/svg/Code";
 
 export default function Home() {
   return (
@@ -9,16 +10,17 @@ export default function Home() {
       <PageFadeTransition>
         <div className="flex flex-col justify-center items-center min-h-[calc(100vh-var(--header-h))]">
           <PaperFan>
-            <a href="./About.jsx">
+            <Link to="/about" className="block w-full h-full">
               <div className="flex flex-col items-center h-full">
-                <h1 className="p-5 pt-8 text-5xl font-sans text-[var(--accentPrimary)] uppercase tracking-widest">
-                  Kaja Emilie Berg
+                <h1 className="p-5 pt-20 text-5xl font-sans text-[var(--accentPrimary)] uppercase tracking-widest">
+                  Name
                 </h1>
                 <h2 className="text-2xl text-[var(--textSecondary)] font-arial uppercase">
                   Full-stack web developer
                 </h2>
+                 <Code className="w-36 mt-24" />
               </div>
-            </a>
+            </Link>
           </PaperFan>
         </div>
       </PageFadeTransition >
